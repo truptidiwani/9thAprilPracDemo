b13f8135d628e67a198e97eb043c82f7982bdde2
truptidiwani <diwanitrupti@gmail.com>
2022-07-28 20:16:33 +0530

    New Project
    Project has been created and just clone it.

---
+++ GitPracProject/src/myPackage/TestBase.java
@@ -0,0 +1,10 @@
+package myPackage;
+
+public class TestBase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Common Properties");
+	}
+
+}
