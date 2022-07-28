b309fbca1efb52361a786f0b879d5c2c055271d3
truptidiwani <diwanitrupti@gmail.com>
2022-07-28 20:46:45 +0530

    HomePage
    I have completed my task  Home Page
    Please do review it

---
+++ GitPracProject/src/Package1/HomePage.java
@@ -0,0 +1,10 @@
+package Package1;
+
+public class HomePage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my home page");
+	}
+
+}
